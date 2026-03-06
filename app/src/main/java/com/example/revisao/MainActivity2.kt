606be495efc8935b76ec91384a72bd0e26c7bf85
20232tiimi0071 <amandafilgueiras07@gmail.com>
2026-03-06 15:39:19 -0300

    Versão Final da Revisão

--- app/src/main/java/com/example/revisao/MainActivity2.kt
+++ app/src/main/java/com/example/revisao/MainActivity2.kt
@@ -5,12 +5,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.revisao.databinding.ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
+    private lateinit var binding: ActivityMain2Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main2)
+
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        val loginUsuario = intent.getStringExtra("LOGIN_USUARIO") ?: "Desconhecido"
+        binding.txtUsuario.text = "Bem-vindo (a), $loginUsuario!"
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
